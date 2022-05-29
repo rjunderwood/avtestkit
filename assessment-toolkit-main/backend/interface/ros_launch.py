04b12e7dc49557688a1956039c0f454f103dbd80
Thiago Sandeiro <102810162@student.swin.edu.au>
2022-05-29 21:44:49 +1000

    Update ros_launch.py
    Co-Authored-By: rjunderwood <29243316+rjunderwood@users.noreply.github.com>

--- assessment-toolkit-main/backend/interface/ros_launch.py
+++ assessment-toolkit-main/backend/interface/ros_launch.py
@@ -1,11 +1,12 @@
-import subprocess as sp
+import os
 
 class ROSLaunch:
     
     __ros__ = None
 
     def __init__(self):
-        self.__ros__ = sp.check_call("sudo ~/ROS_DATA/carla-autoware/run.sh -s", shell=True)
+        self.__ros__ = os.system("gnome-terminal -e 'bash -c \"sudo ~/ROS_DATA/carla-autoware/run.sh -s; exec bash\"'")
+        
 
     def get_ros_launch(self):
         return self.__ros__
