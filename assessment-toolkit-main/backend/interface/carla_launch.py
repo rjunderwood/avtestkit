792d1bbbdea38bfebb7d260ff0ad49569b8ed643
Thiago Sandeiro <102810162@student.swin.edu.au>
2022-05-29 21:02:52 +1000

    Update carla_launch.py
    Co-Authored-By: rjunderwood <29243316+rjunderwood@users.noreply.github.com>

--- assessment-toolkit-main/backend/interface/carla_launch.py
+++ assessment-toolkit-main/backend/interface/carla_launch.py
@@ -2,8 +2,10 @@ import subprocess as sp
 
 class CarlaLaunch:
     
-    __carla__
-
+    __carla__ = None
 
     def __init__(self):
-        __carla__ = sp.check_call("~/CARLA_AUTOWARE/CarlaUE4.sh", shell=True)
+        self.__carla__ = sp.check_call("~/CARLA_AUTOWARE/CarlaUE4.sh", shell=True)
+
+    def get_carla_launch(self):
+        return self.__carla__
