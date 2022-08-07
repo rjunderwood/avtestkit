7a03eca13347096331f3d7f1eb7809d5b5e6c55f
rjunderwood <rileyunderwoodbusiness@gmail.com>
2022-08-07 18:18:16 +1000

    Updated for correct format editing for scenarios

--- assessment_toolkit/ros_patch/run-simulation.bash
+++ assessment_toolkit/ros_patch/run-simulation.bash
@@ -1,18 +1,5 @@
 !/bin/bash
-# #FOLLOW VEHICLE
 roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town01 spawn_point:='338.761,-320.678,0.2,0,0,90'
-
-# #PEDESTRIAN CROSSING
-# roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 spawn_point:='105,63,2.94,0.2,0,0,90'
-# PID=$!
-
+PID=$!
 sleep 10s
 kill $PID
-
-
-#!/bin/bash
-#roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 spawn_point:='105,63,0.2,0,0,90'
-#PID=$!
-
-#sleep 10s
-#kill $PID
