3475440003564183b809e43622f64ccb414f7a5a
riley <rileyunderwoodbusiness@gmail.com>
2022-10-01 20:20:37 +1000

    Extra test PCPVM NPC vehicle arrive same time

--- assessment_toolkit/backend/scenario/pedestrian_crossing_prior_vehicle_manouver.py
+++ assessment_toolkit/backend/scenario/pedestrian_crossing_prior_vehicle_manouver.py
@@ -49,7 +49,7 @@ class ScenarioPedestrianCrossingPriorVehicleManouver:
     from backend.util.spectator_camera import setup_spectator_camera
     
     #How long the scenario actually should run once recording is triggered. 
-    RUNNING_TIME = 30
+    RUNNING_TIME = 70
     scenario_finished = False
     world=None
     blueprint_library=None
