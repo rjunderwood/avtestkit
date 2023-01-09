4b12f21ae46bb7b249942c191033da8619b607fe
riley <rileyunderwoodbusiness@gmail.com>
2023-01-09 18:15:41 +1100

    Add pcpvm

--- assessment_toolkit/ros_patch/run-simulation.bash
+++ assessment_toolkit/ros_patch/run-simulation.bash
@@ -1,5 +1,5 @@
 !/bin/bash
-roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town01 spawn_point:='338.761, -230, 0.2,0,0,90'
+roslaunch carla_autoware_agent carla_autoware_agent.launch town:=Town03 spawn_point:='2,-180,0.2,0.2,0,90'
 PID=$!
 sleep 10s
 kill $PID
